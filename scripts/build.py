3a6463200a3a6c803e5cab4a47bc39efde58745f
liblaf <30631553+liblaf@users.noreply.github.com>
2024-06-15 15:03:53 +0800

    fix: refactor rule set export and save logic
    Refactored the export and save logic for rule sets to improve readability and maintainability. This change organizes the rule set handling into more descriptive variables and functions, making it easier to manage and extend in the future.

--- scripts/build.py
+++ scripts/build.py
@@ -6,19 +6,23 @@ from sbr.rule_set import RuleSet
 URL_PREFIX: str = "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest"
 geosite = Geosite(url=f"{URL_PREFIX}/geosite.db")
 
-geosite_ai: RuleSet = geosite.export("bing", "google", "openai", "perplexity")
-geosite_ai.save("rule-sets/ai.srs")
-geosite_emby: RuleSet = RuleSet.from_url(
-    "https://github.com/NotSFC/rulelist/raw/main/sing-box/Emby/Emby.json"
-)
-geosite_emby.save("rule-sets/emby.srs")
-geosite_onedrive: RuleSet = geosite.export("onedrive")
-geosite_onedrive.save("rule-sets/onedrive.srs")
-geosite_proxy: RuleSet = geosite.export(
+proxy: RuleSet = geosite.export(
     *[category for category in geosite.list() if re.match(r".*!cn$", category)]
 )
-geosite_proxy.save("rule-sets/proxy.srs")
-geosite_cn: RuleSet = geosite.export(
+proxy.save("rule-sets/proxy.srs")
+
+cn: RuleSet = geosite.export(
     *[category for category in geosite.list() if re.match(r"(.*[-@])?cn$", category)]
 )
-geosite_cn.save("rule-sets/cn.srs")
+cn.save("rule-sets/cn.srs")
+
+ai: RuleSet = geosite.export("bing", "google", "openai", "perplexity")
+ai.save("rule-sets/ai.srs")
+
+emby: RuleSet = RuleSet.from_url(
+    "https://github.com/NotSFC/rulelist/raw/main/sing-box/Emby/Emby.json"
+)
+emby.save("rule-sets/emby.srs")
+
+media: RuleSet = geosite.export("netflix", "youtube")
+media.save("rule-sets/media.srs")
